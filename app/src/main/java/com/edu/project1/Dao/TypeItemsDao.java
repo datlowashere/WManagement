923038bcb72c240850d2b011ae738121f38a3e40
datlowashere <datlowashere@gmail.com>
2022-11-30 23:54:57 +0700

    type items dao

--- app/src/main/java/com/edu/project1/Dao/TypeItemsDao.java
+++ app/src/main/java/com/edu/project1/Dao/TypeItemsDao.java
@@ -20,12 +20,7 @@ public class TypeItemsDao {
         DBHelper dbHelper=new DBHelper(context);
         db=dbHelper.getReadableDatabase();
     }
-    String createTBLoaiHang="create table LoaiHang(" +
-            "maLoaiHang integer primary key autoincrement," +
-            "tenLoaiHang text," +
-            "username text references User(username))";
-
-//    them loai hang
+//    Thêm loại hàng
     public  long insert(TypeItems obj){
         ContentValues values=new ContentValues();
         values.put("tenLoaiHang",obj.getTenLoaiHang());
@@ -33,13 +28,13 @@ public class TypeItemsDao {
         return db.insert("LoaiHang",null,values);
     }
 
-//    sua loai hang
+//    Sửa loại hàng
     public int update(TypeItems obj){
         ContentValues values=new ContentValues();
         values.put("tenLoaiHang",obj.getTenLoaiHang());
         return db.update("LoaiHang",values,"maLoaiHang=?",new String[]{String.valueOf(obj.getMaLoaiHang())});
     }
-//    xoa loai hang
+//    xóa loại hàng
     public int delete(String id){
         return db.delete("LoaiHang","maLoaiHang=?",new String[]{id});
     }
@@ -69,34 +64,12 @@ public class TypeItemsDao {
         String sql="select * from LoaiHang where username=?";
         return getData(sql,id);
     }
-//    lay toan bo ten hang theo nguoi dung
-    public String[]getArrTenLoai(String id){
-        String sql="select tenLoaiHang from LoaiHang where username=?";
-        ArrayList<String>arr=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,new String[]{id});
-        while (c.moveToNext()){
-            c.moveToFirst();
-            arr.add(c.getString(0));
-        }
-        return (String[]) arr.toArray();
-
-    }
 
 //    search theo ten
     @SuppressLint("Range")
     public List<TypeItems> searchByName(String name){
-        list=new ArrayList<>();
         String sql="select * from LoaiHang where tenLoaiHang =?";
-        Cursor c=db.rawQuery(sql,new String[]{name});
-        while (c.moveToNext()){
-            c.moveToFirst();
-            TypeItems obj=new TypeItems();
-            obj.setMaLoaiHang(c.getInt(c.getColumnIndex("maLoaiHang")));
-            obj.setTenLoaiHang(c.getString(c.getColumnIndex("tenLoaiHang")));
-            obj.setUsername(c.getString(c.getColumnIndex("username")));
-            list.add(obj);
-        }
-        return list;
+        return getData(sql,name);
     }
 
 }
