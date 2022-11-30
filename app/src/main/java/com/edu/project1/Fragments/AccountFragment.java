0872a165df696d8e58e9c1be70c89954bcfc53e5
datlowashere <datlowashere@gmail.com>
2022-11-30 23:55:18 +0700

    account fragment

--- app/src/main/java/com/edu/project1/Fragments/AccountFragment.java
+++ app/src/main/java/com/edu/project1/Fragments/AccountFragment.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.Gravity;
@@ -206,5 +209,11 @@ public class AccountFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        if (Build.VERSION.SDK_INT >= 26) {
+            ft.setReorderingAllowed(false);
+        }
+        ft.detach(this).attach(this).commit();
+
     }
 }
