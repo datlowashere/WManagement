bb3d12013000bd2bf4ed7ed3b0c5d4c55d3032c9
datlowashere <datlowashere@gmail.com>
2022-12-04 02:46:26 +0700

    report

--- app/src/main/java/com/edu/project1/Dao/ReportDao.java
+++ app/src/main/java/com/edu/project1/Dao/ReportDao.java
@@ -8,23 +8,29 @@ import android.database.sqlite.SQLiteDatabase;
 import com.edu.project1.DBHelper.DBHelper;
 import com.edu.project1.Models.InventoryItems;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ReportDao {
-    private SQLiteDatabase db;
     private List<InventoryItems> list;
+    SQLiteDatabase db;
+    DBHelper dbHelper;
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
     public ReportDao(Context context) {
-        DBHelper dbHelper=new DBHelper(context);
-        db=dbHelper.getWritableDatabase();
+        this.dbHelper=new DBHelper(context);
     }
 //    hang ton
     @SuppressLint("Range")
     public List<InventoryItems> getAllHangTon(String name){
+        db=dbHelper.getReadableDatabase();
         list=new ArrayList<>();
         String sql="select tenHang,tenLoaiHang,soLuongNhap,soLuongXuat,(soLuongNhap-soLuongXuat) as conLai from XuatHang where username=? and conLai>0";
-        Cursor c=db.rawQuery(sql,new String[]{name});
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{name});
         while (c.moveToNext()){
             InventoryItems obj=new InventoryItems();
             obj.setTenHang(c.getString(c.getColumnIndex("tenHang")));
@@ -34,66 +40,141 @@ public class ReportDao {
         };
         return list;
     }
-//   tong so luong nhap hang
-    public int getTongHangNhap(){
-        String sql="select sum(soLuongNhap) as tongSoNhap from NhapHang";
-        List<Integer> tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,null);
-        do{
-            c.moveToFirst();
-            tong.add(c.getInt(0));
-        }while (c.moveToNext());
-        return tong.get(0);
+
+//    ds tien nhap theo giai doan
+    @SuppressLint("Range")
+    public Float[] getDSTienNhapTheoGiaiDoan(String username, String tuNgay, String denNgay){
+        db=dbHelper.getReadableDatabase();
+        ArrayList<Float>tong=new ArrayList<>();
+        String sql="SELECT ngayNhapHang, sum( (donGia * soLuongNhap)) as tien from NhapHang WHERE  username=? and ngayNhapHang BETWEEN ? and ? GROUP BY ngayNhapHang ORDER by ngayNhapHang";
+        Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+        if(c.moveToFirst()){
+            while (!c.isAfterLast()){
+                tong.add(c.getFloat(1));
+                c.moveToNext();
+            };
+        }
+        return tong.toArray(new Float[tong.size()]);
     }
-//    tong so luong xuat hang
-    public int getTongHangXuat(){
-        String sql="select sum(soLuongXuat) as tongSoXuat from XuatHang";
-        List<Integer>tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,null);
-        do{
-            c.moveToFirst();
-            tong.add(c.getInt(0));
-        }while (c.moveToNext());
-        return tong.get(0);
+    @SuppressLint("Range")
+    public String[] getDSNgayNhapHangTheoGiaiDoan(String username, String tuNgay, String denNgay) throws ParseException {
+        db=dbHelper.getReadableDatabase();
+        ArrayList<String> allDate=new ArrayList<>();
+        String sql="SELECT ngayNhapHang as tien from NhapHang WHERE  username=? and ngayNhapHang BETWEEN ? and ? GROUP BY ngayNhapHang ORDER by ngayNhapHang";
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+        if(c.moveToFirst()){
+            while (!c.isAfterLast()){
+                allDate.add(c.getString(0));
+                c.moveToNext();
+            };
+        }
+        return allDate.toArray(new String[allDate.size()]);
     }
-//    tong tien nhap hang
-    public float getTongTienNhap(){
-        String sql="select sum(donGia) from NhapHang";
-        List<Float> tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,null);
-        do{
+
+    //    ds tien nhap theo giai doan
+    public Float[] getDSTienXuatTheoGiaiDoan(String username,String tuNgay,String denNgay){
+        db=dbHelper.getReadableDatabase();
+        ArrayList<Float>tong=new ArrayList<>();
+        String sql="SELECT ngayXuatHang,sum( (donGiaXuat * soLuongXuat)) as tien from XuatHang WHERE  username=? and ngayXuatHang BETWEEN ? and ? GROUP BY ngayXuatHang ORDER by ngayXuatHang ";
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+//        if(c!=null){
+//            if(c.moveToFirst()){
+//                do{
+//                    tong.add((float) c.getInt(1));
+//                }while (c.moveToNext());
+//            }
+//            c.close();
+//        }
+        if(c.moveToFirst()){
+            while (!c.isAfterLast()){
+                tong.add(c.getFloat(1));
+                c.moveToNext();
+            };
+        }
+        return tong.toArray(new Float[tong.size()]);
+    }
+    @SuppressLint("Range")
+    public String[] getDSNgayXuatHangTheoGiaiDoan(String username, String tuNgay, String denNgay) throws ParseException {
+        db=dbHelper.getReadableDatabase();
+        ArrayList<String> allDate=new ArrayList<>();
+        String sql="SELECT ngayXuatHang from XuatHang WHERE username=? and ngayXuatHang BETWEEN ? and ? GROUP BY ngayXuatHang  ORDER by ngayXuatHang";
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+//        if(c!=null){
+//            if(c.moveToFirst()){
+//                do{
+//                    allDate.add(c.getString(0));
+//                }while (c.moveToNext());
+//            }
+//            c.close();
+//        }
+        if(c.moveToFirst()){
+            while (!c.isAfterLast()){
+                allDate.add(c.getString(0));
+                c.moveToNext();
+            };
+        }
+
+        return allDate.toArray(new String[allDate.size()]);
+    }
+
+    //    tien nhap theo giai doan(2 khoang ngay)
+    public float getTongTienNhapTheoGiaiDoan(String username,String tuNgay,String denNgay){
+        db=dbHelper.getReadableDatabase();
+        String sql="select  sum(donGia*soLuongNhap) as tonTien from NhapHang where username=? and ngayNhapHang between ? and ?";
+        ArrayList<Float> tong=new ArrayList<>();
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+        while (c.moveToNext()){
             tong.add(c.getFloat(0));
-        }while (c.moveToNext());
-        return tong.get(0);
-    }
-// tong tien xuat
-    public float getTongTienXuat(){
-        String sql="select sum(donGiaXuat) tongTienXuat from XuatHang";
-        List<Float> tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,null);
-        do{
-            tong.add(c.getFloat(0));
-        }while (c.moveToNext());
-        return tong.get(0);
-    }
-//    tien nhap theo giai doan(2 khoang ngay)
-    public float getTienNhapTheoGiaiDoan(String tuNgay,String denNgay){
-        String sql="select  sum(donGia) as tonTien from NhapHang where ngayNhapHang between ? and ?";
-        List<Float> tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,new String[]{tuNgay,denNgay});
-        do{
-            tong.add(c.getFloat(0));
-        }while (c.moveToNext());
+        }
         return tong.get(0);
     }
     //    tien xuat theo giai doan(2 khoang ngay)
-    public float getTienXuatTheoGaiDoan(String tuNgay,String denNgay){
-        String sql="select  sum(donGiaXuat) as tonTien from XuatHang where ngayXuatHang between ? and ?";
-        List<Float> tong=new ArrayList<>();
-        Cursor c=db.rawQuery(sql,new String[]{tuNgay,denNgay});
-        do{
+    public float getTongTienXuatTheoGaiDoan(String username,String tuNgay,String denNgay){
+        db=dbHelper.getReadableDatabase();
+        String sql="select  sum(donGiaXuat*soLuongXuat) as tonTien from XuatHang where username=? and ngayXuatHang between ? and ?";
+        ArrayList<Float> tong=new ArrayList<>();
+        @SuppressLint("Recycle") Cursor c=db.rawQuery(sql,new String[]{username,tuNgay,denNgay});
+        while (c.moveToNext()){
             tong.add(c.getFloat(0));
-        }while (c.moveToNext());
+        };
         return tong.get(0);
     }
+
+//    tong tien nhap, xuat theo gia doan
+    public float[]getTongNhapXuatTheoGiaiDoan(String username,String tuNgay,String denNgay){
+        db=dbHelper.getReadableDatabase();
+        float nhap=0,xuat=0;
+        Cursor cn=db.rawQuery("select  sum(donGia*soLuongNhap) as tonTien from NhapHang where username=? and ngayNhapHang between ? and ?",new String[]{username,tuNgay,denNgay});
+        if(cn.getCount()!=0){
+            cn.moveToFirst();
+            nhap=cn.getFloat(0);
+        }
+        Cursor cx=db.rawQuery("select  sum(donGiaXuat*soLuongXuat) as tonTien from XuatHang where username=? and ngayXuatHang between ? and ?",new String[]{username,tuNgay,denNgay});
+        if(cx.getCount()!=0){
+            cx.moveToFirst();
+            xuat=cx.getFloat(0);
+        }
+        float[] tong=new float[]{nhap,xuat};
+        return tong;
+    }
+//    tong nhap+xuat theo giai doan
+
+    public float getTongTienNhapXuatTheoGiaiDoan(String username,String tuNgay,String denNgay){
+        db=dbHelper.getReadableDatabase();
+        float nhap=0,xuat=0;
+        Cursor cn=db.rawQuery("select  sum(donGia*soLuongNhap) as tonTien from NhapHang where username=? and ngayNhapHang between ? and ?",new String[]{username,tuNgay,denNgay});
+        if(cn.getCount()!=0){
+            cn.moveToFirst();
+            nhap=cn.getFloat(0);
+        }
+        Cursor cx=db.rawQuery("select  sum(donGiaXuat*soLuongXuat) as tonTien from XuatHang where username=? and ngayXuatHang between ? and ?",new String[]{username,tuNgay,denNgay});
+        if(cx.getCount()!=0){
+            cx.moveToFirst();
+            xuat=cx.getFloat(0);
+        }
+        float tong=nhap+xuat;
+        return tong;
+    }
+
+
 }
--- app/src/main/java/com/edu/project1/Fragments/RevenueReportFragment.java
+++ app/src/main/java/com/edu/project1/Fragments/RevenueReportFragment.java
@@ -1,5 +1,7 @@
 package com.edu.project1.Fragments;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +9,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import com.edu.project1.Dao.ReportDao;
+import com.edu.project1.Helper.CustomToasts;
+import com.edu.project1.MainActivity;
 import com.edu.project1.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 
 public class RevenueReportFragment extends Fragment {
 
+    private TextView tvTuNgay,tvDenNgay;
+    private ReportDao dao;
+    private BarChart barchartTQ,barChartNhap,barChartXuat;
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    CustomToasts customToasts=new CustomToasts();
+
+
 
     public RevenueReportFragment() {
         // Required empty public constructor
-    }
-
-    public static RevenueReportFragment newInstance(String param1, String param2) {
-        RevenueReportFragment fragment = new RevenueReportFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -34,6 +57,225 @@ public class RevenueReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_revenue_report, container, false);
+        View view=inflater.inflate(R.layout.fragment_revenue_report, container, false);
+        tvTuNgay=view.findViewById(R.id.tvTuNgay);
+        tvDenNgay=view.findViewById(R.id.tvDenNgay);
+        barChartNhap=view.findViewById(R.id.barchartNhapKho);
+        barChartXuat=view.findViewById(R.id.barchartXuatKho);
+        barchartTQ=view.findViewById(R.id.barchartTongQuan);
+
+        tvTuNgay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogDatePicker(tvTuNgay);
+            }
+        });
+        tvDenNgay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogDatePicker(tvDenNgay);
+            }
+        });
+        view.findViewById(R.id.btnDoanhThu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tvTuNgay.getText().toString().isEmpty() || tvDenNgay.getText().toString().isEmpty()){
+                    customToasts.warningToast(getContext(),"Chưa chọn khoảng ngày");
+                }else{
+                    try {
+                        if(sdf.parse(tvTuNgay.getText().toString()).after(sdf.parse(tvDenNgay.getText().toString()))){
+                            customToasts.warningToast(getContext(),"Ngày bắt đầu phải trước ngày kết thúc");
+                        }else{
+                            customToasts.successToast(getContext(),"Kéo xuống để check doanh thu :3");
+                            barchartTQ();
+                            barchartNhapKho();
+                            barchartXuatKho();
+                        }
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        return view;
     }
+    private void dialogDatePicker(TextView tv){
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        tv.setText(year  + "-"+(monthOfYear + 1) +"-" + dayOfMonth);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+    private void barchartTQ(){
+        dao=new ReportDao(getContext());
+        ArrayList<BarEntry> entries=new ArrayList<>();
+
+        float[] dsTien=dao.getTongNhapXuatTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        float tong=dao.getTongTienNhapXuatTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        String[] ten={"Nhập Kho","Xuất Kho"};
+
+        for(int i=0;i<dsTien.length;i++){
+            entries.add(new BarEntry(i,dsTien[i]));
+        }
+        BarDataSet barDataSet=new BarDataSet(entries,"Tổng: "+tong+" đ");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData barData=new BarData(barDataSet);
+        barchartTQ.setData(barData);
+        barchartTQ.notifyDataSetChanged();
+
+        XAxis xAxis=barchartTQ.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(ten));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setCenterAxisLabels(false);
+        xAxis.setLabelCount(ten.length);
+        xAxis.setLabelRotationAngle(70);
+
+
+        YAxis yAxis=barchartTQ.getAxis(YAxis.AxisDependency.LEFT);
+        YAxis yAxis1=barchartTQ.getAxis(YAxis.AxisDependency.RIGHT);
+
+        yAxis1.setStartAtZero(true);
+        yAxis.setStartAtZero(true);
+        yAxis.setDrawGridLines(false);
+
+        yAxis.setGranularity(1.0f);
+        yAxis.setGranularityEnabled(true);
+
+        yAxis1.setGranularity(1.0f);
+        yAxis1.setGranularityEnabled(true);
+
+        barchartTQ.getDescription().setEnabled(true);
+        barchartTQ.getDescription().setText("VNĐ");
+        barchartTQ.getDescription().setPosition(100, 50);
+        barchartTQ.setFitBars(true);
+        barchartTQ.animateY(3000);
+
+    }
+
+    private void barchartNhapKho() {
+        dao=new ReportDao(getContext());
+        ArrayList<BarEntry> entries=new ArrayList<>();
+
+        Float[] dsTien=dao.getDSTienNhapTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        String[] dsNgay= new String[0];
+        try {
+            dsNgay = dao.getDSNgayNhapHangTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Float tong=dao.getTongTienNhapTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+
+        for(int i=0;i<dsTien.length;i++){
+            entries.add(new BarEntry(i,dsTien[i]));
+        }
+        BarDataSet barDataSet=new BarDataSet(entries,"Tổng: "+tong+" đ");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData barData=new BarData(barDataSet);
+        barChartNhap.setData(barData);
+        barChartNhap.notifyDataSetChanged();
+
+        XAxis xAxis=barChartNhap.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(dsNgay));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setCenterAxisLabels(false);
+        xAxis.setLabelCount(dsNgay.length);
+        xAxis.setLabelRotationAngle(70);
+
+
+        YAxis yAxis=barChartNhap.getAxis(YAxis.AxisDependency.LEFT);
+        YAxis yAxis1=barChartNhap.getAxis(YAxis.AxisDependency.RIGHT);
+
+        yAxis1.setStartAtZero(true);
+        yAxis.setStartAtZero(true);
+        yAxis.setDrawGridLines(false);
+
+        yAxis.setGranularity(1.0f);
+        yAxis.setGranularityEnabled(true);
+
+        yAxis1.setGranularity(1.0f);
+        yAxis1.setGranularityEnabled(true);
+
+        barChartNhap.getDescription().setEnabled(true);
+        barChartNhap.getDescription().setText("VNĐ");
+        barChartNhap.getDescription().setPosition(100, 50);
+        barChartNhap.setFitBars(true);
+        barChartNhap.animateY(3000);
+
+
+    }
+    private void barchartXuatKho(){
+        dao=new ReportDao(getContext());
+        ArrayList<BarEntry> entries=new ArrayList<>();
+
+        Float[] dsTien=dao.getDSTienXuatTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        String[] dsNgay= new String[0];
+        try {
+            dsNgay = dao.getDSNgayXuatHangTheoGiaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Float tong=dao.getTongTienXuatTheoGaiDoan(getUsername(),tvTuNgay.getText().toString(),tvDenNgay.getText().toString());
+
+        for(int i=0;i<dsTien.length;i++){
+            entries.add(new BarEntry(i,dsTien[i]));
+        }
+        BarDataSet barDataSet=new BarDataSet(entries,"Tổng: "+tong+" đ");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData barData=new BarData(barDataSet);
+        barChartXuat.setData(barData);
+        barChartXuat.notifyDataSetChanged();
+
+        XAxis xAxis=barChartXuat.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(dsNgay));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setCenterAxisLabels(false);
+        xAxis.setLabelCount(dsNgay.length);
+        xAxis.setLabelRotationAngle(70);
+
+
+        YAxis yAxis=barChartXuat.getAxis(YAxis.AxisDependency.LEFT);
+        YAxis yAxis1=barChartXuat.getAxis(YAxis.AxisDependency.RIGHT);
+
+        yAxis1.setStartAtZero(true);
+        yAxis.setStartAtZero(true);
+        yAxis.setDrawGridLines(false);
+
+        yAxis.setGranularity(1.0f);
+        yAxis.setGranularityEnabled(true);
+
+        yAxis1.setGranularity(1.0f);
+        yAxis1.setGranularityEnabled(true);
+
+        barChartXuat.getDescription().setEnabled(true);
+        barChartXuat.getDescription().setText("VNĐ");
+        barChartXuat.getDescription().setPosition(100, 50);
+        barChartXuat.setFitBars(true);
+        barChartXuat.animateY(3000);
+
+    }
+
+    private String getUsername(){
+        MainActivity activity=(MainActivity)getActivity();
+        String username=activity.getUsername();
+        return username;
+    }
+
 }
